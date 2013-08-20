00002ecfd459e19f44ead19e6a0a486f807f0005
Mark Wotton <mwotton@gmail.com>
2013-08-20 10:24:52 +1000

    reorg

--- dnscap.py
+++ dnscap.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
-#DiabloHorn http://diablohorn.wordpress.com
+# based in part on
+# http://diablohorn.wordpress.com/2010/12/05/dnscat-traffic-post-dissector/
 
 import sys
-import md5
 from compiler.ast import flatten
 from datetime import datetime,timedelta
-#http://stackoverflow.com/questions/13249341/surpress-scapy-warning-message-when-importing-the-module
+
 import logging
 import bernhard
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 import os
+from socket import gethostname
 
 
-riemann = bernhard.Client(host=os.getenv('RIEMANN_HOST'), transport=bernhard.UDPTransport)
 
 try:
     #use this if the other line doesn't work
@@ -27,84 +27,82 @@ except:
 
 conf.verb = 0
 
-from socket import gethostname
-
-
 host = gethostname()
 
 def usage():
-    print 'DiabloHorn http://diablohorn.wordpress.com'
-    print 'verify nmap scans, find delayed responses'
+    print 'dnscap'
+    print 'forward dns query packets to riemann for visualisation'
     print 'Usage: '
-    print sys.argv[0] + ' <pcapfile> <threshold>'
-    print 'Ex: '
-    print sys.argv[0] + ' delayedresponse.pcap 0.8'
-    print '[timestamp] [difference] [src ip] [dst ip] [dst port] [response flags]'
-    print '1367887784.231386 5.00098395348 10.50.0.107 10.50.0.103 22 [\'SYN\', \'ACK\']'
+    print sys.argv[0] + ' <pcapfile>'
     sys.exit()
 
-def gethash(data):
-    return md5.new(data).hexdigest()
+class PacketLog:
+    def __init__(self, riemann):
+        self.riemann=riemann
+        self.reset()
 
-def flush(timing,orphan):
-    events = []
-    ts = flatten([x for x in timing.values()])
-    avg = sum(ts) / len(ts)
-    events.append(bernhard.Event(params={'host': host, 'service': 'dns_average', 'metric': (sum(ts)/len(ts))}))
-    events.append(bernhard.Event(params={'host': host, 'service': 'dns_max', 'metric': max(ts)}))
-    events.append(bernhard.Event(params={'host': host, 'service': 'dns_count', 'metric': len(ts)}))
-    events.append(bernhard.Event(params={'host': host, 'service': 'dns_orphans', 'metric': orphans}))
+    def reset():
+        self.timing = {}
+        self.orphans = 0
+        self.queries = {}
 
-    riemann.transmit(bernhard.Message(events=events))
+    def flush():
+        events = []
+        ts = flatten([x for x in self.timing.values()])
+        avg = sum(ts) / len(ts)
+        events.append(bernhard.Event(params={'host': host, 'service': 'dns_average', 'metric': (sum(ts)/len(ts))}))
+        events.append(bernhard.Event(params={'host': host, 'service': 'dns_max', 'metric': max(ts)}))
+        events.append(bernhard.Event(params={'host': host, 'service': 'dns_count', 'metric': len(ts)}))
+        events.append(bernhard.Event(params={'host': host, 'service': 'dns_orphans', 'metric': orphans}))
+
+        riemann.transmit(bernhard.Message(events=events))
+
+
+
+    def process_packet(packet):
+        if not packet.haslayer(DNS):
+            return
+        dns = packet[DNS]
+        print packet[IP].time
+        print dns.id
+        if packet.haslayer(DNSRR):
+            r = packet[DNSRR]
+            print "it's a response: " + str(packet[DNS].id)
+            try:
+                [query, oldtime] = self.queries.pop(dns.id)
+                # FIX check query name is the same too
+                if query.qname not in timing:
+                    self.timing[query.qname] = []
+
+                self.timing[query.qname].append(packet[IP].time - oldtime)
+            except KeyError:
+                # either someone is sending us fake responses
+                # or we have duplicates
+                self.orphans = self.orphans + 1
+        elif packet.haslayer(DNSQR):
+            q=packet[DNSQR]
+            self.queries[dns.id] = [q, packet[IP].time]
+            print "it's a query: " + packet[DNSQR].qname
+            print self.packet[DNSQR].fields
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         usage()
-    queries = {}
-    timing = {}
-    orphans = 0
-    #should enable us to read large files
+    # in our use case this is often a FIFO
     pcapdata = PcapReader(sys.argv[1])
-    print queries
-    print timing
     next_clearance = datetime.now() + timedelta(seconds=1)
+    riemann = bernhard.Client(host=os.getenv('RIEMANN_HOST'), transport=bernhard.UDPTransport)
+    packetlog = new PacketLog(riemann)
+
     for packet in pcapdata:
         now = datetime.now()
         if now > next_clearance:
             next_clearance = datetime.now() + timedelta(seconds=1)
-            flush(timing,orphans)
-            orphans = 0
-            for k in timing.keys():
-                del(timing[k])
+            packetlog.flush()
+        packetlog.process_packet(packet)
 
 
-
-        if packet.haslayer(DNS):
-            dns = packet[DNS]
-            print packet[IP].time
-            print dns.id
-            if packet.haslayer(DNSRR):
-                r = packet[DNSRR]
-                print "it's a response: " + str(packet[DNS].id)
-                try:
-                    [query, oldtime] = queries.pop(dns.id)
-                    # FIX check query name is the same too
-                    if query.qname not in timing:
-                        timing[query.qname] = []
-
-                    timing[query.qname].append(packet[IP].time - oldtime)
-                    # times[
-                except KeyError:
-                        # either someone is sending us fake responses
-                        # or we have duplicates
-                    orphans = orphans + 1
-
-            elif packet.haslayer(DNSQR):
-                q=packet[DNSQR]
-                queries[dns.id] = [q, packet[IP].time]
-                print "it's a query: " + packet[DNSQR].qname
-                print packet[DNSQR].fields
 
     print "finished"
 
@@ -112,4 +110,4 @@ if __name__ == "__main__":
         print "no response for "
         print [k,queries[v]]
 
-    flush(timing,orphans)
+    packetlog.flush()
